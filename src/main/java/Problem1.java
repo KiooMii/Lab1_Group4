e90ff9121da89b1e96ee0b9b533bc937bc1205f2
KiooMii <kio.mingote@gmail.com>
2022-08-15 00:42:08 +0800

    new commit
    Problem 1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,4 +1,25 @@
+import java.util.*;
 
 public class Problem1{
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        List<Integer> Integer = new ArrayList<Integer>();
+        System.out.println("Enter a series of number to compute for the sum of all the integers. Input '0' to stop");
+        
+        int num = sc.nextInt();
+        while(num != 0){
+            Integer.add(num);
+            num = sc.nextInt();
+        }
+        
+        int sum = 0;
+        for(int i = 0; i < Integer.size(); i++){
+            sum += Integer.get(i);
+        }
+        
+        System.out.println(sum);
+        
+    }
     
 }

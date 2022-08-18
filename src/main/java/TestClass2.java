e653abc54f0d434da6bc8bc02103cba23140836b
KiooMii <kio.mingote@gmail.com>
2022-08-18 15:44:59 +0800

    Update TestClass2.java

--- src/main/java/TestClass2.java
+++ src/main/java/TestClass2.java
@@ -2,71 +2,7 @@ import java.util.*;
 public class TestClass2 {
     public static void main(String args[]){
         
-        TreeMap<Date, String> syllabus = new TreeMap<>();
-        int i = 1;
-        Date aug1 = new Date(9,1, 2002);
-        Date aug2 = new Date(9,2, 2002);
-        Date aug3 = new Date(9,3, 2002);
-        Date aug4 = new Date(9,4, 2002);
-        Date aug5 = new Date(9,5, 2002);
-        Date aug6 = new Date(9,6, 2002);
-        Date aug7 = new Date(9,7, 2002);
-        Date aug8 = new Date(9,8, 2002);
-        Date aug9 = new Date(9,9, 2002);
-        Date aug10 = new Date(9,10, 2002);
-        Date aug11 = new Date(9,11, 2002);
-        Date aug12 = new Date(9,12, 2002);
-        Date aug13 = new Date(9,13, 2002);
-        Date aug14 = new Date(9,14, 2002);
-        Date aug15 = new Date(9,15, 2002);
-        Date aug16 = new Date(9,16, 2002);
-        Date aug17 = new Date(9,17, 2002);
-        Date aug18 = new Date(9,18, 2002);
-        Date aug19 = new Date(9,19, 2002);
-        Date aug20 = new Date(9,20, 2002);
-        Date aug21= new Date(9,21, 2002);
-        Date aug22 = new Date(9,22, 2002);
-        Date aug23 = new Date(9,23, 2002);
-        Date aug24 = new Date(9,24, 2002);
-        Date aug25 = new Date(9,25, 2002);
-        Date aug26 = new Date(9,26, 2002);
-        Date aug27 = new Date(9,27, 2002);
-        Date aug28 = new Date(9,28, 2002);
-        Date aug29 = new Date(9,29, 2002);
-        Date aug30 = new Date(9,30, 2002);
         
-        syllabus.put(aug1, aug1.toString());
-        syllabus.put(aug2, aug2.toString());
-        syllabus.put(aug3, aug3.toString());
-        syllabus.put(aug4, aug4.toString());
-        syllabus.put(aug5, aug5.toString());
-        syllabus.put(aug6, aug6.toString());
-        syllabus.put(aug7, aug7.toString());
-        syllabus.put(aug8, aug8.toString());
-        syllabus.put(aug9, aug9.toString());
-        syllabus.put(aug10, aug10.toString());
-        syllabus.put(aug11, aug11.toString());
-        syllabus.put(aug12, aug12.toString());
-        syllabus.put(aug13, aug13.toString());
-        syllabus.put(aug14, aug14.toString());
-        syllabus.put(aug15, aug15.toString());
-        syllabus.put(aug16, aug16.toString());
-        syllabus.put(aug17, aug17.toString());
-        syllabus.put(aug18, aug18.toString());
-        syllabus.put(aug19, aug19.toString());
-        syllabus.put(aug20, aug20.toString());
-        syllabus.put(aug21, aug21.toString());
-        syllabus.put(aug22, aug22.toString());
-        syllabus.put(aug23, aug23.toString());
-        syllabus.put(aug24, aug24.toString());
-        syllabus.put(aug25, aug25.toString());
-        syllabus.put(aug26, aug26.toString());
-        syllabus.put(aug27, aug27.toString());
-        syllabus.put(aug28, aug28.toString());
-        syllabus.put(aug29, aug29.toString());
-        syllabus.put(aug30, aug30.toString());
-        
-        System.out.print(syllabus.keySet());
         
         
         
